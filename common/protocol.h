c82467e9d6423cccbd7f76eb1914f69fc2cd100c
Garson-hu <guangx.hu@foxmail.com>
2026-03-12 14:35:42 -0400

    DPU RDMA info

--- common/protocol.h
+++ common/protocol.h
@@ -16,3 +16,12 @@ struct HostMemInfo {
     char desc_str[256];
     BufferType type;         // Mark this is primary or mirror buffer
 } __attribute__((packed));
+
+
+struct DpuRdmaInfo {
+    uint32_t qp_num;     // Network RC QP number, unique for each DPU
+    uint16_t lid;        // Local Identifier
+    uint8_t  gid[16];    // Global Identifier (RoCEv2)
+    uint32_t rkey;       // RKey of the remote DPU's ring_buf
+    uint64_t vaddr;      // Start address of the remote DPU's ring_buf
+} __attribute__((packed));
